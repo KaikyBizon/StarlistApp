416343dd3273ce7b522f18d227fc69e5a2b32bd8
Gabriel Candreva <andrade.gabrielcandreva@gmail.com>
2024-08-20 14:23:13 -0300

    aprimoramento no no layout da pagina de planos e conheça-nos da landingpage

--- frontend/src/components/LandingPage/Conheca.jsx
+++ frontend/src/components/LandingPage/Conheca.jsx
@@ -3,25 +3,33 @@ function Conheca() {
     return (
         <>
             <section id="conhecer">
-                <h1>Conheça-nos</h1>
+                <h1>CONHEÇA-NOS</h1>
                 <div className="conhecer-grid">
                     <div className="conhecer-cards">
                         <h2>Suporte</h2>
-                        <p className="explicação">Ajuda e suporte 24h para auxiliar <br /> nas suas dúvidas e problemas</p>
-                        <img className="img-conhecer" src="/public/images/Suporte.png" alt="" />
+                        <p className="explicação">Ajuda e suporte 24h para auxiliar nas suas dúvidas e problemas.</p>
+                        <img className="img-conhecer" src="/public/images/Active Support-bro.png" alt="" />
+                    </div>
+
+                    <div className="conhecer-cards">
+                        <h2>Usabilidade</h2>
+                        <p className="explicação"> Tem um design simples que ajuda na visualização
+                            das tarefas e faz com que o usuario tenha uma experiencia rapida e pratica.
+                        </p>
+                        <img className="img-conhecer" src="/public/images/Search engines-bro.png" alt="" />
                     </div>
 
                     <div className="conhecer-cards">
                         <h2>Utilidades</h2>
-                        <p className="explicação">Te ajuda no planejamento, <br />organização e gerenciamento das <br />suas ideias com
-                            o principal foco <br /> em te ajudar a aproveitar <br /> melhor seu tempo. </p>
-                        <img className="img-conhecer" src="/public/images/Utilidade.png" alt="" />
+                        <p className="explicação">Te ajuda a planejar, organizar e gerenciar as suas ideias com
+                            o principal foco em te ajudar a aproveitar melhor seu tempo. </p>
+                        <img className="img-conhecer" src="/public/images/To do list-bro.png" alt="" />
                     </div>
 
                     <div className="conhecer-cards">
                         <h2>Segurança</h2>
-                        <p className="explicação">Seus planejamentos e dados <br />protegidos com total segurança</p>
-                        <img className="img-conhecer" src="/public/images/seguranca.jpeg" alt="" />
+                        <p className="explicação">Seus planejamentos e dados protegidos com total segurança.</p>
+                        <img className="img-conhecer" src="/public/images/Security On-bro.png" alt="" />
                     </div>
 
 
