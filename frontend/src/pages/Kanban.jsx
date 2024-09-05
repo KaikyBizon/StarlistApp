df86d731cebc2ea03c3042e67b9b02f06e5d752f
Leticia Bueno <leticiabuenosoares.lb@gmail.com>
2024-09-05 08:48:17 -0300

    Atualização lista kanban

--- frontend/src/pages/Kanban.jsx
+++ frontend/src/pages/Kanban.jsx
@@ -1,4 +1,3 @@
-import Geral from '../components/Geral';
 import Menu from '../components/menu';
 import Options from '../components/Options';
 import Formulario from '../components/Formulario';
@@ -6,22 +5,51 @@ import { useState, useEffect } from 'react';
 import '../StylesPages/kanban.css';
 
 function Kanban({ onListaSalva }) {
-    const [lista, setLista] = useState([]);
-    const [categorias, setCategorias] = useState([]);
-    const [tarefasPorCategoria, setTarefasPorCategoria] = useState({});
-    const [novaLista, setNovaLista] = useState('');
-    const [exibirFormulario, setExibirFormulario] = useState(false)
+    const [lista, setLista] = useState([]); // Define um estado 'lista' como um array vazio, utilizado para armazenar as listas.
+    const [categorias, setCategorias] = useState([]);// Define um estado 'categorias' como um array vazio, utilizado para armazenar as categorias.
+    const [tarefasPorCategoria, setTarefasPorCategoria] = useState({});// Define um estado 'tarefasPorCategoria' como um objeto vazio, utilizado para armazenar as tarefas organizadas por categoria.
+    const [novaLista, setNovaLista] = useState('');// Define um estado 'novaLista' como uma string vazia, utilizado para armazenar o nome de uma nova lista a ser criada.
+    const [exibirFormulario, setExibirFormulario] = useState(false);// Define um estado 'exibirFormulario' como 'false', utilizado para controlar a visibilidade do formulário para criar uma nova lista.
+    const [modalVisivel, setModalVisivel] = useState(false);// Define um estado 'modalVisivel' como 'false', utilizado para controlar a visibilidade de um modal (por exemplo, confirmação de exclusão).
+    const [listaParaExcluir, setListaParaExcluir] = useState(null);// Define um estado 'listaParaExcluir' como 'null', utilizado para armazenar o ID da lista que está sendo selecionada para exclusão.
+    const [showFormulario, setShowFormulario] = useState(false); // Controle de exibição do formulário
+    const [tarefaSelecionada, setTarefaSelecionada] = useState(null); // Tarefa selecionada para edição
+    const [editandoListaId, setEditandoListaId] = useState(null); // Estado para controlar qual lista está sendo editada
+    const [nomeEditado, setNomeEditado] = useState(''); // Estado para armazenar o novo nome da lista
 
+    // Define um estado 'dadosList' como um objeto inicial com campos 'nome', 'tarefa_id', e 'usuario_id'.
+    // 'nome' é uma string vazia, enquanto 'tarefa_id' e 'usuario_id' são obtidos do 'localStorage'.
     const [dadosList, setDadosList] = useState({
         nome: '',
         tarefa_id: localStorage.getItem("ID"),
         usuario_id: localStorage.getItem("ID")
     });
 
+    /**
+ * Nome da função: fetchTarefasParaCategoria
+ * 
+ * Autor: Leticia e Thaina
+ * 
+ * Data de criação: 03 de setembro de 2024
+ * 
+ * Parâmetros de entrada:
+ *   - `categoriaId`: string ou número - ID da categoria cujas tarefas devem ser buscadas.
+ * 
+ * Retorno:
+ *   - `Promise`: Retorna uma promessa que resolve para:
+ *     - Um array de objetos com as tarefas associadas à categoria, se a requisição for bem-sucedida.
+ *     - Um array vazio, caso ocorra um erro na requisição ou se a resposta não for bem-sucedida (status diferente de 2xx).
+ * 
+ * Descrição/observações:
+ *   - Esta função faz uma requisição HTTP GET para um endpoint que busca as tarefas associadas a uma categoria específica, identificada pelo `categoriaId`.
+ *   - A resposta da requisição é tratada para garantir que, em caso de sucesso (`resposta.ok`), os dados das tarefas sejam retornados.
+ *   - Em caso de erro, a função captura e loga o erro no console, retornando um array vazio como fallback.
+ *   - A URL utilizada no fetch inclui o `categoriaId` como parâmetro de rota para especificar a categoria desejada.
+ */
     // Função para buscar tarefas para uma categoria específica
     const fetchTarefasParaCategoria = async (categoriaId) => {
         try {
-            const resposta = await fetch(`http://10.135.60.19:8085/tarefas/${categoriaId}`, {
+            const resposta = await fetch(`http://10.135.60.22:8085/tarefas/${categoriaId}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -43,11 +71,34 @@ function Kanban({ onListaSalva }) {
         }
     };
 
+    /**
+ * Nome da função: fetchCategoriasETarefas
+ * 
+ * Autor: Leticia e Thaina
+ * 
+ * Data de criação: 03 de setembro de 2024
+ * 
+ * Parâmetros de entrada:
+ *   - Nenhum parâmetro de entrada. A função utiliza o ID do usuário armazenado no `localStorage`.
+ * 
+ * Retorno:
+ *   - Nenhum retorno direto. A função atualiza os estados de categorias e tarefas associadas com o resultado da busca.
+ * 
+ * Descrição/observações:
+ *   - Esta função faz uma requisição HTTP GET para buscar as categorias associadas ao usuário atual (identificado pelo `usuarioId` armazenado no `localStorage`).
+ *   - Se a resposta for bem-sucedida (`resposta.ok`), a função atualiza o estado `categorias` com o resultado obtido.
+ *   - Em seguida, para cada categoria obtida, a função faz outra requisição para buscar as tarefas associadas àquela categoria, utilizando a função `fetchTarefasParaCategoria`.
+ *   - As requisições para buscar as tarefas são executadas em paralelo utilizando `Promise.all`, garantindo que todas as promessas sejam resolvidas antes de continuar.
+ *   - Os resultados das tarefas são armazenados em um objeto (`tarefasMap`), onde cada chave corresponde ao ID de uma categoria, e o valor associado é o array de tarefas daquela categoria.
+ *   - O estado `tarefasPorCategoria` é então atualizado com esse mapeamento de tarefas.
+ *   - Em caso de erro na busca das categorias ou das tarefas, os erros são capturados e logados no console.
+ */
+
     // Função para buscar categorias e tarefas
     const fetchCategoriasETarefas = async () => {
         const usuarioId = localStorage.getItem('ID');
         try {
-            const resposta = await fetch(`http://10.135.60.19:8085/lista/${usuarioId}`, {
+            const resposta = await fetch(`http://10.135.60.22:8085/lista/${usuarioId}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -79,10 +130,37 @@ function Kanban({ onListaSalva }) {
         }
     };
 
+    // Define a função 'handleChange' que será chamada quando houver uma mudança no valor de um campo de entrada (input).
     const handleChange = (event) => {
+        // Atualiza o estado 'novaLista' com o novo valor do campo de entrada, extraído do evento (event.target.value).
         setNovaLista(event.target.value);
     };
 
+    /**
+ * Nome da função: handleSubmit
+ * 
+ * Autor: Thaina e Leticia
+ * 
+ * Data de criação: 03 de setembro de 2024
+ * 
+ * Parâmetros de entrada:
+ *   - `e`: Event - Evento de submissão do formulário, utilizado para prevenir o comportamento padrão de recarregar a página.
+ * 
+ * Retorno:
+ *   - Nenhum retorno direto. A função lida com a criação de uma nova lista e atualiza o estado da aplicação conforme o resultado da requisição.
+ * 
+ * Descrição/observações:
+ *   - Esta função é chamada ao submeter um formulário para criar uma nova lista.
+ *   - Primeiro, a função previne o comportamento padrão do formulário (`e.preventDefault()`).
+ *   - Em seguida, verifica se o nome da nova lista não está vazio. Se estiver, uma mensagem de erro é definida e a função retorna.
+ *   - A função então faz uma requisição HTTP POST para o endpoint `/receber-dados`, enviando os dados da nova lista a ser criada.
+ *   - O corpo da requisição contém uma ação (`acao`) especificada como 'criar_lista' e os dados da lista (`dadosList`), com o nome da nova lista sobrescrevendo o nome existente.
+ *   - Se a resposta não for bem-sucedida (`resposta.ok`) ou se houver mensagens de erro na resposta, essas mensagens de erro são definidas no estado `mensagensErro`.
+ *   - Se a criação da lista for bem-sucedida, o estado `lista` é atualizado com a nova lista, e o nome da nova lista é resetado (`setNovaLista('')`).
+ *   - A nova lista também é salva no `localStorage` para persistência.
+ *   - Se a função `onListaSalva` estiver definida, ela é chamada após a lista ser salva com sucesso, permitindo que outras ações sejam realizadas (como atualizar uma interface de usuário).
+ *   - Em caso de erro durante a requisição, o erro é capturado e logado no console.
+ */
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (!novaLista.trim()) {
@@ -91,7 +169,7 @@ function Kanban({ onListaSalva }) {
         }
 
         try {
-            const resposta = await fetch('http://10.135.60.19:8085/receber-dados', {
+            const resposta = await fetch('http://10.135.60.22:8085/receber-dados', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -126,9 +204,39 @@ function Kanban({ onListaSalva }) {
         }
     };
 
+    // Define a função assíncrona 'handleDeleteLista' que será chamada ao tentar excluir uma lista, recebendo 'listaId' como parâmetro.
     const handleDeleteLista = async (listaId) => {
+        // Define o estado 'listaParaExcluir' com o ID da lista que se deseja excluir.
+        setListaParaExcluir(listaId);
+        // Define o estado 'modalVisivel' como 'true' para exibir o modal de confirmação de exclusão.
+        setModalVisivel(true);
+    };
+
+    /**
+ * Nome da função: confirmarExclusao
+ * 
+ * Autor: Thaina e Leticia
+ * 
+ * Data de criação: 03 de setembro de 2024
+ * 
+ * Parâmetros de entrada:
+ *   - Nenhum parâmetro de entrada direto. A função utiliza variáveis de estado, como `listaParaExcluir`, para determinar a lista que será excluída.
+ * 
+ * Retorno:
+ *   - Nenhum retorno direto. A função lida com a exclusão de uma lista e atualiza os estados da aplicação de acordo com o resultado da operação.
+ * 
+ * Descrição/observações:
+ *   - Esta função é chamada para confirmar a exclusão de uma lista específica.
+ *   - Ela faz uma requisição HTTP DELETE para o endpoint `/lista/${listaParaExcluir}`, onde `listaParaExcluir` é o ID da lista a ser excluída.
+ *   - Se a resposta da requisição for bem-sucedida (`resposta.ok`), a lista excluída é removida do estado `categorias` utilizando o método `filter`.
+ *   - O estado `tarefasPorCategoria` também é atualizado para remover as tarefas associadas à lista excluída.
+ *   - Se a exclusão falhar, uma mensagem de erro é registrada no console.
+ *   - Após a exclusão (ou em caso de erro), o modal de confirmação é fechado, e o estado `listaParaExcluir` é resetado para `null`.
+ *   - Em caso de erro durante a requisição, o erro é capturado e logado no console.
+ */
+    const confirmarExclusao = async () => {
         try {
-            const resposta = await fetch(`http://10.135.60.19:8085/lista/${listaId}`, {
+            const resposta = await fetch(`http://10.135.60.22:8085/lista/${listaParaExcluir}`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
@@ -137,58 +245,86 @@ function Kanban({ onListaSalva }) {
 
             if (resposta.ok) {
                 // Filtra a lista removendo a excluída
-                setCategorias(categorias.filter(categoria => categoria.id !== listaId));
+                setCategorias(categorias.filter(categoria => categoria.id !== listaParaExcluir));
                 // Atualiza o estado das tarefas
                 const novoTarefasPorCategoria = { ...tarefasPorCategoria };
-                delete novoTarefasPorCategoria[listaId];
+                delete novoTarefasPorCategoria[listaParaExcluir];
                 setTarefasPorCategoria(novoTarefasPorCategoria);
             } else {
                 console.error('Erro ao excluir lista');
             }
+
+            // Fecha o modal após a ação
+            setModalVisivel(false);
+            setListaParaExcluir(null);
         } catch (error) {
             console.error('Erro ao excluir lista:', error);
         }
     };
 
+    // Define a função 'cancelarExclusao' que será chamada para cancelar a exclusão de uma lista.
+    const cancelarExclusao = () => {
+        // Define o estado 'modalVisivel' como 'false' para esconder o modal de confirmação de exclusão.
+        setModalVisivel(false);
+        // Define o estado 'listaParaExcluir' como 'null' para limpar o ID da lista que estava marcada para exclusão.
+        setListaParaExcluir(null);
+    };
+
+    // Define a função 'handleClearInput' que será chamada para limpar o campo de entrada da nova lista.
     const handleClearInput = () => {
+        // Define o estado 'novaLista' como uma string vazia, efetivamente limpando o campo de entrada.
         setNovaLista('');
     };
 
+    // Define a função 'handleExibirFormulario' que será chamada para exibir o formulário de edição de uma lista específica.
     const handleExibirFormulario = (listaId) => {
+        // Define o estado 'exibirFormulario' com o ID da lista para a qual o formulário deve ser exibido.
         setExibirFormulario(listaId);
     };
 
-
+    // Define um hook 'useEffect' que será executado assim que o componente for montado (carregado pela primeira vez).
     useEffect(() => {
+        // Chama a função 'fetchCategoriasETarefas' para buscar categorias e tarefas ao montar o componente.
         fetchCategoriasETarefas();
+        // O array vazio '[]' indica que este 'useEffect' só será executado uma vez, na montagem do componente.
     }, []);
+
+    const handleEditarClick = (tarefa) => {
+        setTarefaSelecionada(tarefa); // Define a tarefa a ser editada
+        setShowFormulario(true); // Exibe o formulário
+    };
+
+    const handleFecharFormulario = () => {
+        setShowFormulario(false); // Fecha o formulário
+        setTarefaSelecionada(null); // Limpa a tarefa selecionada
+    };
 
     return (
         <>
             <Menu />
             <Options />
-            <section className='interacao'>
-                <section className='calendario-left'>
-                    <Geral />
-                </section>
-
                 <section className='cartoes-kanban'>
                     {categorias.map((categoria) => (
                         <section key={categoria.id} className="status-kanban">
                             <div className="kanban">
                                 <div className='titulo-lista'>
                                     <h4 className='status-tarefa'>{categoria.nome}</h4>
+                                    <img className='editar-nomeLista' src="../../public/images/editar_lista.png" alt="" />
                                 </div>
                                 {tarefasPorCategoria[categoria.id] && tarefasPorCategoria[categoria.id].map((tarefa, index) => (
                                     <div key={index} className="tarefa-item">
-                                        <h5 className='titulo-tarefa'>{tarefa.titulo}</h5>
+                                        <div className='titulo-editar'>
+                                            <h5 className='titulo-tarefa'>{tarefa.titulo}</h5>
+                                            <img className='editar-lista' src="../../public/images/editar_lista.png" alt="" onClick={() => handleEditarClick(tarefa)} />
+                                        </div>
                                         <p className='data-hora'>Data: {tarefa.data}</p>
                                         <p className='data-hora'>Hora: {tarefa.horario}</p>
                                     </div>
                                 ))}
                                 <div className="formulario-fixo">
-                                    <button onClick={() => handleExibirFormulario(categoria.id)}>Nova tarefa</button>
+                                    <button onClick={() => handleExibirFormulario(categoria.id)} className='nova-tarefa'>Nova tarefa</button>
                                     {exibirFormulario === categoria.id && <Formulario onClose={handleExibirFormulario} listaId={categoria.id} />}
+                                    <img src="../../public/images/lixeira.png" alt="" onClick={() => handleDeleteLista(categoria.id)} className='excluir'/>
                                 </div>
                             </div>
                         </section>
@@ -201,7 +337,25 @@ function Kanban({ onListaSalva }) {
                         </div>
                     </div>
                 </section>
-            </section>
+
+            {modalVisivel && (
+                <div className="modal-confirmacao">
+                    <div className="modalContent">
+                        <p>Deseja mesmo excluir a lista e suas tarefas?</p>
+                        <div>
+                            <button className="botoesConfirmarExclusao" onClick={confirmarExclusao}>Confirmar</button>
+                            <button className="botoesConfirmarExclusao" onClick={cancelarExclusao}>Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {showFormulario && (
+                <Formulario
+                    tarefa={tarefaSelecionada} // Passa a tarefa selecionada para o formulário
+                    onClose={handleFecharFormulario} // Função para fechar o formulário
+                />
+            )}
         </>
     );
 }
