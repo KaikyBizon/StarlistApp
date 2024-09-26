f734f9283ea5861f0048a8fa81f6a91355a09bd7
Leticia Bueno <leticiabuenosoares.lb@gmail.com>
2024-09-26 12:14:53 -0300

    Alterações css

--- frontend/src/components/Options.jsx
+++ frontend/src/components/Options.jsx
@@ -6,55 +6,8 @@ import AddParticipantes from './AddParticipantes.jsx';
 import { Link, useLocation } from 'react-router-dom'; // Importar useLocation
 
 function Options() {
-    const defaultName = 'Nome da lista';
-    const [showModal, setShowModal] = useState(false);
-    const [recipientName, setRecipientName] = useState(defaultName);
-    const [tempName, setTempName] = useState(defaultName);
-    const [error, setError] = useState('');
     const [showFormulario, setShowFormulario] = useState(false);
-
     const location = useLocation(); // Usar useLocation para obter o caminho atual
-
-    useEffect(() => {
-        const modalState = localStorage.getItem('modalVisible');
-        const name = localStorage.getItem('recipientName');
-
-        if (modalState !== null) {
-            setShowModal(JSON.parse(modalState));
-        }
-        if (name !== null) {
-            setRecipientName(name);
-            setTempName(name);
-        } else {
-            setRecipientName(defaultName);
-            setTempName(defaultName);
-        }
-    }, []);
-
-    const handleShow = () => {
-        setShowModal(true);
-        localStorage.setItem('modalVisible', JSON.stringify(true));
-    };
-
-    const handleClose = () => {
-        setShowModal(false);
-        localStorage.setItem('modalVisible', JSON.stringify(false));
-    };
-
-    const handleInputChange = (event) => {
-        setTempName(event.target.value);
-        setError('');
-    };
-
-    const handleSave = () => {
-        if (tempName.trim() === '') {
-            setError('O nome da lista não pode estar vazio.');
-            return;
-        }
-        setRecipientName(tempName);
-        localStorage.setItem('recipientName', tempName);
-        handleClose();
-    };
 
     const handleShowFormulario = () => {
         setShowFormulario(true);
@@ -69,24 +22,13 @@ function Options() {
             <Navbar expand="lg" className="bg-body-tertiary nav-fixed">
                 <Container>
                     <div className='nav-utensilios'>
-                        <Navbar.Brand href="#home" onClick={handleShow}>{recipientName}</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
+                                <Link to="/kanban">Kanban</Link>
+                                <Link to="/todo">ToDo</Link>
                                 <Link to="/gerenciarpart">Participantes</Link>
                                 <Link to="/gerenciar-equipe">Equipe</Link>
-                                <NavDropdown className='drop-options' title="Opções" id="basic-nav-dropdown">
-                                    <AddParticipantes>
-                                        <NavDropdown.Item href="#action/3.1">Adicionar participantes</NavDropdown.Item>
-                                    </AddParticipantes>
-                                    <NavDropdown.Divider />
-                                    <Link to="/kanban">
-                                        <NavDropdown.Item href="#action/3.3">KanBan</NavDropdown.Item>
-                                    </Link>
-                                    <Link to="/todo">
-                                        <NavDropdown.Item href="#action/3.4">ToDo</NavDropdown.Item>
-                                    </Link>
-                                </NavDropdown>
                             </Nav>
                         </Navbar.Collapse>
                     </div>
@@ -98,31 +40,6 @@ function Options() {
                     </div>
                 </Container>
             </Navbar>
-
-            <Modal show={showModal} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Altere o título da lista</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form>
-                        <Form.Group controlId="formRecipientName">
-                            <Form.Label>Título da lista:</Form.Label>
-                            <Form.Control
-                                className='inputlista'
-                                type="text"
-                                value={tempName}
-                                onChange={handleInputChange}
-                                style={{ boxShadow: 'none', borderColor: 'initial' }}
-                            />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button className='fecharlista' variant='tertiary' onClick={handleClose}>Fechar</Button>
-                    <Button className='salvarlistanome' variant="primary" onClick={handleSave}>Salvar mudanças</Button>
-                </Modal.Footer>
-            </Modal>
 
             {showFormulario && (
                 <Formulario onClose={handleCloseFormulario} />
--- frontend/src/pages/Gerenciarpart.jsx
+++ frontend/src/pages/Gerenciarpart.jsx
@@ -5,16 +5,19 @@ import '../StylesPages/gerenciarpart.css'
 function Gerenciarpart() {
     return (
         <>
-            <Cabecalho/>
+            <Cabecalho />
             <section id="gerenciar-part">
                 <div id="title-gerenciar">
                     <h1>Gerenciar participantes</h1>
                 </div>
-                <div className="cards-gerenciar">
-                    <Cardgerenciar />
-                    <Cardgerenciar />
-                    <Cardgerenciar />
-                    <Cardgerenciar />
+                <div className="estrutura">
+                    <div className="cards-gerenciar">
+                        <Cardgerenciar />
+                        <Cardgerenciar />
+                        <Cardgerenciar />
+                        <Cardgerenciar />
+                    </div>
+                    <button className="addPart" type="button" name="botao" value="addPart" >Adicionar participantes</button>
                 </div>
             </section>
         </>
