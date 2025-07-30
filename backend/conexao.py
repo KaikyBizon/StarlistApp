f6664ee04244463dc6624ab6f044f051ab687651
KaikyBizon <kaiky.bizon@gmail.com>
2025-07-30 19:13:28 -0300

    fix: Correção dos valores de conexão do mySQL de localhost parao banco deployado

--- backend/conexao.py
+++ backend/conexao.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def conectar():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="starlist"
+        host="s37lph.h.filess.io",
+        user="Starlist_industrial",
+        password="402fed95e5c58d448acb3dea4306860cec0d9785",
+        database="Starlist_industrial"
     )
